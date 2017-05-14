1a764fd00298f75b2ffe1feb1eb657f684c87f15
Alex Ander <loktionov129@gmail.com>
2017-05-14 09:36:30 +0700

    Add files via upload

--- modules/mymodule/include.php
+++ modules/mymodule/include.php
@@ -1,18 +1,4 @@
 <?
-class myeventhandler
-{
-    function MyOnBuildGlobalMenu(&$aGlobalMenu, &$aModuleMenu) 
-    { 
-        foreach($aModuleMenu as $k => $v)
-        { 
-            if($v["parent_menu"] == "global_menu_settings" && $v["items_id"] == "menu_users")
-            { 
-                $aModuleMenu[$k]["items"][] = Array(
-                    "text" => "Кастомный пункт пользователей",
-                    "url" => "user_custom.php?lang=".LANG,
-                    "title" => "Своя страница пользователей"
-                ); 
-            } 
-        } 
-    }
-}
+
+require_once('classes/myeventhandler.php');
+require_once('classes/CModuleOptions.php');
