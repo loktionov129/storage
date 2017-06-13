dce4b8d1265401e406e746fcf3cfd5867f0100ca
Alex Ander <loktionov129@gmail.com>
2017-06-13 21:23:21 +0700

    Delete index.php

--- modules/d7dull/lang/ru/install/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$MESS['BEX_D7DULL_MODULE_NAME'] = 'D7-образец';
-$MESS['BEX_D7DULL_MODULE_DESCRIPTION'] = 'Модуль-болванка, написанный на новом ядре Битрикса.';
-$MESS['BEX_D7DULL_MODULE_PARTNER_NAME'] = 'Битрикс-эксперты';
