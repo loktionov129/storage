fa6a6d6c6112a127cff7fb50e8c31ab69a0925f2
Alex Ander <loktionov129@gmail.com>
2017-07-01 12:28:40 +0700

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,59 +1,49 @@
 'use strict';
 
 /* Gulp plugins */
-var gulp = require('gulp'), // Task runner
-    notify = require('gulp-notify'),
+const gulp = require('gulp'), // Task runner
+    gutil = require('gulp-util'), // Utility functions for gulp plugins
+    plumber = require('gulp-plumber'), // Prevent pipe breaking caused by errors from gulp plugins
+    notify = require('gulp-notify'), // Notification plugin for gulp
     watch = require('gulp-watch'), // Watch, that actually is an endless stream
     rename = require('gulp-rename'), // Rename files
     del = require('del'), // Delete something
-    path = require('path'),
     concat = require('gulp-concat'), // Concatenates files
     htmlmin = require('gulp-htmlmin'),
     less = require('gulp-less'), // Compile Less to CSS
-    lessReporter = require('gulp-less-reporter'), // Error reporter for gulp-less
     autoprefixer = require('gulp-autoprefixer'), // Prefix CSS
-    //csscomb = require('gulp-csscomb'), // Coding style formatter for CSS
     minifycss = require('gulp-minify-css'), // Minify CSS
     babel = require('gulp-babel'),
     uglify = require('gulp-uglify'), // Minify JS
-    //jshint = require('gulp-jshint'), // JS code linter
-    //stylish = require('jshint-stylish'), // Reporter for JSHint
+    jshint = require('gulp-jshint'), // JS code linter
+    stylish = require('jshint-stylish'), // Reporter for JSHint
     runSequence = require('run-sequence').use(gulp), // Run a series of dependent gulp tasks in order
     sourcemaps = require('gulp-sourcemaps'), // Write source maps
     browserSync = require('browser-sync'), // Synchronised browser testing
     reload = browserSync.reload,
     size = require('gulp-size'); // Display the size of something
 
-/* Path settings */
-var projectPath = {
-    build: { // Set build paths
-        html: 'build/',
-        js: 'build/js/',
-        css: 'build/css/'
-    },
-    src: { // Set source paths
-        html: 'src/index.html',
-        js: 'src/js/app.js',
-        style: 'src/styles/style.less'
-    },
-    watch: { // Set watch paths
-        html: 'src/index.html',
-        js: 'src/js/app.js',
-        style: 'src/styles/style.less'
-    },
-    clean: ['build/**/*', '!build/.gitignore', '!build/favicon.ico'], // Set paths and exludes for cleaning build dir
-};
-
-/* BrowserSync local web server settings */
-var config = {
-    server: {
-        baseDir: './build'
-    },
-    tunnel: true,
-    host: 'localhost',
-    port: 9000,
-    injectChanges: true,
-    logPrefix: 'BrowserSync'
+/* Project settings */
+const config = {
+    production: !!gutil.env.production,
+    path: {
+        build: { // Set build paths
+            html: 'build/',
+            js: 'build/js/',
+            css: 'build/css/'
+        },
+        src: { // Set source paths
+            html: 'src/index.html',
+            js: 'src/js/app.js',
+            style: 'src/styles/style.less'
+        },
+        watch: { // Set watch paths
+            html: 'src/index.html',
+            js: 'src/js/app.js',
+            style: 'src/styles/style.less'
+        },
+        clean: ['build/**/*', '!build/.gitignore', '!build/favicon.ico'], // Set paths and exludes for cleaning build dir
+    }
 };
 
 /*********/
@@ -78,74 +68,116 @@ gulp.task('build', function(cb) {
 
 /* Clean build directory */
 gulp.task('clean', function () {
-    del(projectPath.clean);
+    del(config.path.clean);
 });
 
 /* HTML */
 gulp.task('html', function () {
-    return gulp.src(projectPath.src.html)
+    return gulp.src(config.path.src.html)
         .pipe(htmlmin({collapseWhitespace: true}))
-        .pipe(size({
-            title: 'HTML'
-        }))
-        .pipe(gulp.dest(projectPath.build.html))
+        .pipe(size({title: 'HTML'}))
+        .pipe(gulp.dest(config.path.build.html))
         .pipe(reload({stream: true}));
 });
 
 /* JavaScript */
-gulp.task('js', function () {
-   return gulp.src(projectPath.src.js)
-        .pipe(sourcemaps.init())
-        .pipe(babel({
-            presets: ['es2015']
+gulp.task('js', function() {
+    return gulp.src(config.path.src.js)
+        .pipe(plumber({
+            errorHandler: function (error) {
+                const message = error.message || '';
+                const errName = error.name || '';
+                const codeFrame = error.codeFrame || '';
+                gutil.log(gutil.colors.red.bold('[JS Error]')+' '+ gutil.colors.bgRed(errName));
+                gutil.log(gutil.colors.bold('message:') +' '+ message);
+                gutil.log(gutil.colors.bold('codeframe:') + '\n' + codeFrame);
+                notify({
+                    title: "[JS Error] " + errName,
+                    message: message
+                }).write('');
+                this.emit('end');
+            }
         }))
+        .pipe(jshint()).pipe(jshint.reporter(stylish))
+        .pipe(config.production ? gutil.noop() : sourcemaps.init())
+        .pipe(babel({presets: ['es2015']}))
+        //.pipe(gulp.dest(config.path.build.js))
+        .pipe(rename({ suffix: '.min' }))
         .pipe(uglify())
-        .pipe(sourcemaps.write())
-        .pipe(size({
-            title: 'JS'
-        }))
-        .pipe(gulp.dest(projectPath.build.js))
+        .pipe(config.production ? gutil.noop() : sourcemaps.write())
+        .pipe(size({title: 'JS'}))
+        .pipe(gulp.dest(config.path.build.js))
         .pipe(reload({stream: true}));
 });
 
 
 /* Less */
 gulp.task('less', function() {
-    return gulp.src(projectPath.src.style)
-        .pipe(sourcemaps.init())
-        .pipe(less({
-            paths: [ path.join(__dirname, 'less', 'includes') ]
+    return gulp.src(config.path.src.style)
+        .pipe(plumber({
+            errorHandler: function (error) {
+                const type = error.type || '';
+                const message = error.message || '';
+                const extract = error.extract || [];
+                const line = error.line || '';
+                const column = error.column || '';
+                gutil.log(gutil.colors.red.bold('[LESS Error]') +' '+ gutil.colors.bgRed(type +' ('+ line +':'+ column +')'));
+                gutil.log(gutil.colors.bold('message:') +' '+ message);
+                gutil.log(gutil.colors.bold('codeframe:') +'\n'+ extract.join('\n'));
+                notify({
+                    title: "[LESS Error] " + type +' ('+ line +':'+ column +')',
+                    message: message + extract.join('\n')
+                }).write('');
+                this.emit('end');
+            }
         }))
-        .on('error', lessReporter)
+        .pipe(config.production ? gutil.noop() : sourcemaps.init())
+        .pipe(less())
         .pipe(autoprefixer('> 2%'))
-        //.pipe(gulp.dest(projectPath.build.css))
+        //.pipe(gulp.dest(config.path.build.css))
         .pipe(rename({ suffix: '.min' }))
         .pipe(minifycss())
-        .pipe(sourcemaps.write('./'))
-        .pipe(size({
-            title: 'CSS'
-        }))
-        .pipe(gulp.dest(projectPath.build.css))
+        .pipe(config.production ? gutil.noop() : sourcemaps.write('./'))
+        .pipe(size({title: 'CSS'}))
+        .pipe(gulp.dest(config.path.build.css))
         .pipe(reload({stream: true}));
 });
 
 /* Watching */
 gulp.task('watch', ['webserver'], function(){
-    watch([projectPath.watch.html], function(event, cb) {
-        notify("HTML Changed: " + event.path).write('');
+    watch([config.path.watch.html], function(event, cb) {
         gulp.start('html');
     })
-    watch([projectPath.watch.js], function(event, cb) {
-        notify("JS Changed: " + event.path).write('');
+    watch([config.path.watch.js], function(event, cb) {
         gulp.start('js');
     });
-    watch([projectPath.watch.style], function(event, cb) {
-        notify("LESS Changed: " + event.path).write('');
+    watch([config.path.watch.style], function(event, cb) {
         gulp.start('less');
     });
 });
 
 /* BrowserSync local web server*/
 gulp.task('webserver', function () {
-    browserSync(config);
+    browserSync({
+        server: {
+            baseDir: './build'
+        },
+        tunnel: true,
+        host: 'localhost',
+        port: 9000,
+        injectChanges: true,
+        logPrefix: 'BrowserSync'
+    });
+});
+
+/*
+
+[BrowserSync] Localtunnel issue: connection refused: localtunnel.me:40611 (check your firewall settings)
+[BrowserSync] Oops! The localtunnel appears to have disconnected. Reconnecting...
+
+
+*/
+
+gulp.task('test', function(){
+    gutil.log(gutil.env.production);
 });
