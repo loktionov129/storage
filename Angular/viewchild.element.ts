27ddbab0d9061f6e559a68406b701a0f024a3190
Alex Ander <loktionov129@gmail.com>
2017-08-13 22:47:05 +0700

    Update viewchild.element.ts

--- Angular/viewchild.element.ts
+++ Angular/viewchild.element.ts
@@ -38,3 +38,38 @@ export class AppComponent {
 /* С помощью применения к нему декоратора ViewChild к свойству counterComponent мы устанавливаем,
   что это свойство будет содержать объект дочернего компонента, который внедряется через элемент <child-comp></child-comp>.
   И в этом случае мы уже можем не использовать шаблонные переменные в шаблоне.*/
+
+
+/*Привязка ViewChild к шаблонным переменным
+
+Несмотря на то, что выше мы не использовали переменные,
+тем не менее с помощью декоратора ViewChild также можно связать свойство и переменную из шаблона.
+Так, изменим код главного компонента:*/
+
+import { Component, ViewChild } from '@angular/core';
+       
+@Component({
+    selector: 'my-app',
+    template: `<p #nameParagraph>{{name}}</p>
+               <p>{{nameParagraph.textContent}}</p>
+               <button (click)="change()">Изменить</button>`
+})
+export class AppComponent { 
+ 
+    @ViewChild("nameParagraph")
+    nameParagraph: HTMLElement;
+     
+    name: string = "Tom";
+     
+    change() { 
+        console.log(this.nameParagraph.nativeElement.textContent); 
+        this.nameParagraph.nativeElement.textContent = "hell";
+    }
+}
+
+/* Здесь в шаблоне определяется переменная nameParagraph, которая представляет код параграфа.
+А в декоратор ViewChild передается имя этой переменной.
+Поэтому свойство nameParagraph, к которому применяется декоратор, будет указывать на эту переменную.
+
+По нажатию на кнопку выводится и изменяется текстовое содержимое этой переменной.
+*/
