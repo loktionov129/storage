2961d16128e098306742607c38094d0fe4cc4e35
Alex Ander <loktionov129@gmail.com>
2017-10-17 21:43:41 +0700

    Update main.js

--- NodeJS/hhparser/main.js
+++ NodeJS/hhparser/main.js
@@ -8,22 +8,19 @@ const fs = require('fs');
 (async function main(args) {
     if (args.length <= 2) {
         console.log('usage: node main.js company:"facebook"');
-        return
+        return;
     }
     
     const terms = App.prepareTerms(args);
-    
     for (const term of terms) {
         switch (term.key.toLowerCase()) {
             case 'company': {
                 const result = await App.findCompanyByName(term.value);
-                ////
                 var stream = fs.createWriteStream("tmp.txt");
                 stream.once('open', (fd) => {
                     stream.write(result);
                     stream.end();
                 });
-                ////
                 break;
             }
                 
