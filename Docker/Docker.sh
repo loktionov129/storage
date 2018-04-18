0cdddc27b8a7e43a173bafcef9f1589c80b55301
Alex Ander <loktionov129@gmail.com>
2018-04-18 09:04:22 +0700

    Create Docker.sh

---
+++ Docker/Docker.sh
@@ -0,0 +1,10 @@
+docker build
+
+docker run -d --rm -v '/C/projects/wptest':'/www' -p 8081:8081 caskill
+
+docker ps
+docker ps -a
+
+docker exec -it 2614dc047f79 bash
+
+# http://192.168.99.100:8081/
