7bd164e3a797e100b7305bc032435951074a4cd3
Alexander Loktionov <alexander.loktionov@saritasa.com>
2018-05-29 15:21:25 +0700

    Update MultiEnv

--- Docker/MultiEnv/scripts/compose.sh
+++ Docker/MultiEnv/scripts/compose.sh
@@ -2,7 +2,5 @@
 
 ENV=${1:-dev}
 
-#ln -sf $(pwd)/docker/docker-compose.$ENV.yml $(pwd)/docker/docker-compose.override.yml
-#(cd ./docker/ && docker-compose -f docker-compose.yml -f docker-compose.override.yml up -d)
+(cd ./docker/ && docker-compose -f docker-compose.$ENV.yml up -d)
 
-(cd ./docker/ && docker-compose up -d)
