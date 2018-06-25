e090b44de61bb6f25d2f2c97c134eeeb4611efb5
Alexander Loktionov <loktionov129@gmail.com>
2018-06-26 00:06:44 +0800

    Add files via upload

---
+++ bash/sed/replacer.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -e
+set -u
+
+INPUT_FILE=$1
+ENV_FILE=$2
+
+REPLACE_STRING=""
+while IFS="" read -r LINE || [[ -n "$LINE" ]]; do
+    ENV_KEY="$(cut -d"=" -f1 <<<"$LINE")"
+    ENV_VALUE="$(cut -d"=" -f2 <<<"$LINE")"
+    REPLACE_STRING+="s|%$ENV_KEY%|$ENV_VALUE|g;"
+done < $ENV_FILE
+
+REPLACE_STRING=$(echo $REPLACE_STRING | sed 's/.$//')
+sed -i $REPLACE_STRING config.json
