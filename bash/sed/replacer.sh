d702dea669d66c50ddc64cd4025c250a40ffb216
Alexander Loktionov <loktionov129@gmail.com>
2018-06-25 23:27:48 +0700

    fix

--- bash/sed/replacer.sh
+++ bash/sed/replacer.sh
@@ -9,7 +9,7 @@ ENV_FILE=$2
 REPLACE_STRING=""
 while IFS="" read -r LINE || [[ -n "$LINE" ]]; do
     ENV_KEY="$(cut -d"=" -f1 <<<"$LINE")"
-    ENV_VALUE="$(cut -d"=" -f2 <<<"$LINE")"
+    ENV_VALUE="$(cut -d"=" -f 2- <<<"$LINE")"
     REPLACE_STRING+="s|%$ENV_KEY%|$ENV_VALUE|g;"
 done < $ENV_FILE
 
