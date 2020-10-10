b0c9162666b11ffa0cb2662edc7e88cb9ff8b71a
Aleksandr Loktionov <loktionov129@gmail.com>
2020-10-10 17:03:28 +0300

    Update index.ts

--- __devnull__/index.ts
+++ __devnull__/index.ts
@@ -1,4 +1,73 @@
-/* TypeScript */
+// This file is required by karma.conf.js and loads recursively all the .spec and framework files
+
+import { getTestBed } from '@angular/core/testing';
+import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
+
+// tslint:disable-next-line:no-import-side-effect
+import 'zone.js/dist/zone-testing';
+
+// tslint:disable-next-line:no-any
+declare const require: any;
+
+// First, initialize the Angular testing environment.
+getTestBed().initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting());
+
+// Then we find all the tests.
+const context = require.context('./', true, /\.spec\.ts$/);
+
+// And load the modules.
+context.keys().map(context);
+
+if (environment.production) {
+  enableProdMode();
+}
+
+platformBrowserDynamic()
+  .bootstrapModule(AppModule, {
+    defaultEncapsulation: ViewEncapsulation.None,
+    /* ngZone: 'noop', */
+  })
+  .catch(err => console.error(err));
+if (environment.production) {
+  enableProdMode();
+}
+
+platformBrowserDynamic()
+  .bootstrapModule(AppModule, {
+    defaultEncapsulation: ViewEncapsulation.None,
+    /* ngZone: 'noop', */
+  })
+  .catch(err => console.error(err));
+if (environment.production) {
+  enableProdMode();
+}
+
+platformBrowserDynamic()
+  .bootstrapModule(AppModule, {
+    defaultEncapsulation: ViewEncapsulation.None,
+    /* ngZone: 'noop', */
+  })
+  .catch(err => console.error(err));
+if (environment.production) {
+  enableProdMode();
+}
+
+platformBrowserDynamic()
+  .bootstrapModule(AppModule, {
+    defaultEncapsulation: ViewEncapsulation.None,
+    /* ngZone: 'noop', */
+  })
+  .catch(err => console.error(err));
+if (environment.production) {
+  enableProdMode();
+}
+
+platformBrowserDynamic()
+  .bootstrapModule(AppModule, {
+    defaultEncapsulation: ViewEncapsulation.None,
+    /* ngZone: 'noop', */
+  })
+  .catch(err => console.error(err));
 /* TypeScript */
 /* TypeScript */
 /* TypeScript */
