76491c7ef8ea9b2c350943f87f591967a4466f7c
Alexander Loktionov <loktionov129@gmail.com>
2020-12-05 19:42:38 +0300

    F#

--- Backend/f_diez/Program.fs
+++ Backend/f_diez/Program.fs
@@ -2,6 +2,13 @@
 
 module Program =
     // https://dungpa.github.io/fsharp-cheatsheet
+    // https://fsharpforfunandprofit.com/
+
+    // https://github.com/atsapura/CardManagement
+
+    // :telegram:
+    // fsharp_chat, fsharp_jobs
+
     open flow
     open hacks
     open notes
