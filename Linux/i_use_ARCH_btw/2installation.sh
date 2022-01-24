110a5140872372e3632352c1d7e289a91a7403fc
Aleksandr Loktionov <loktionov129@gmail.com>
2022-01-24 22:11:34 +0700

    Update 2installation.sh

--- Linux/i_use_ARCH_btw/2installation.sh
+++ Linux/i_use_ARCH_btw/2installation.sh
@@ -1,3 +1,3 @@
-echo Installation (intel-ucode for intel OR amd-ucode for amd)
+echo "Installation (intel-ucode for intel OR amd-ucode for amd)"
 pacstrap /mnt base linux linux-firmware nano intel-ucode btrfs-progs git
 genfstab -U /mnt >> /mnt/etc/fstab
