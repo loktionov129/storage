9c6d30b609311e7074877d09f2ca0ec03d9024a2
Aleksandr Loktionov <loktionov129@gmail.com>
2022-01-28 19:45:56 +0700

    Update 4configuring.sh

--- Linux/i_use_ARCH_btw/4configuring.sh
+++ Linux/i_use_ARCH_btw/4configuring.sh
@@ -5,18 +5,5 @@ sudo pacman -Syy
 sudo firewall-cmd --add-port=1025-65535/tcp --permanent
 sudo firewall-cmd --add-port=1025-65535/udp --permanent
 sudo firewall-cmd --reload
-
-# Install packages
-sudo pacman -S --noconfirm xorg-xwayland qt5-wayland firefox-developer-edition
-
-# aur
-mkdir -p ~/Projects/aur
-cd ~/Projects/aur
-git clone https://aur.archlinux.org/paru.git
-cd paru
-makepkg -si --noconfirm
-cd ..
-
-# system-backup\restore
-paru -S --noconfirm timeshift timeshift-autosnap nerd-fonts-jetbrains-mono
 sudo systemctl disable getty@tty2.service
+
