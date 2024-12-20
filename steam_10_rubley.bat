3ea7db799567f7a1bbcb3ffa898d4ab40067f9a0
Aleksandr Loktionov <loktionov129@gmail.com>
2024-12-20 19:30:23 +0700

    Update steam_10_rubley.bat

--- steam_10_rubley.bat
+++ steam_10_rubley.bat
@@ -4,4 +4,28 @@ echo win+L    -- go back to main account
 
 echo copy C:\steam\steam.exe -> C:\_xitriy\steam.exe
 echo create bat:
-runas /user:xitriy /SAVECRED "C:\_xitriy\steam.exe"
+
+
+
+@ECHO OFF
+SETLOCAL EnableExtensions
+SET konkyrent=steam.exe
+FOR /F %%x IN ('tasklist /NH /FI "IMAGENAME eq %konkyrent%"') DO IF %%x == %konkyrent% (
+  GOTO Running
+) ELSE (
+  GOTO NotRunning
+)
+
+
+:Running
+ECHO %konkyrent% is Running
+pause
+GOTO End
+
+:NotRunning
+runas /user:lop /SAVECRED "G:\_lop\steam.exe"
+GOTO End
+
+:End
+
+
