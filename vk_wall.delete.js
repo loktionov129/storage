cc7a6ce8126acba4c8783862463b9bc0744a70d7
Aleksandr Loktionov <loktionov129@gmail.com>
2025-06-09 12:51:45 +0700

    Create vk_wall.delete.js

---
+++ vk_wall.delete.js
@@ -0,0 +1,24 @@
+// tab #1
+// https://vk.com/id0
+JSON.stringify([...document.querySelectorAll('.feed_wall--no-islands ._post.post')] // находим все посты на стене
+    .filter(post => [...post.querySelectorAll('a')].find(a => a.textContent === 'vk.com/app3882511#wallpublic')) // фильтруем их
+    .map(post => post.id.split('_')[1])) // получаем идентификаторы постов для удаления
+
+
+
+
+// tab #2
+// https://api.vk.com/method/wall.delete
+// предварительно удаляем вручную один пост, затем во вкладке Network копируем запрос "Copy as fetch"
+// и не забываем обновлять токен
+async function deletePost(id)
+{
+  await fetch("https://api.vk.com/method/wall.delete?v=12313123131312312312313123131313&client_id=12313123131312312312313123131313", {
+        "body": "owner_id=12313123131312312312313123131313&post_id="+id+"&access_token=12313123131312312312313123131313",
+        "method": "POST",
+    });
+    await new Promise(resolve => setTimeout(resolve, 1000));
+}
+var posts =
+["сюда", "подставляем", "список", "идентификаторов", "постов"]
+;(async function(){ for (var post of posts) { await deletePost(post); } })();
