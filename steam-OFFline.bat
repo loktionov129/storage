c3fe59f184a5d146453bdbafdc529559384027b7
Aleksandr Loktionov <loktionov129@gmail.com>
2025-08-23 16:10:25 +0700

    Update steam-OFFline.bat

--- steam-OFFline.bat
+++ steam-OFFline.bat
@@ -1,2 +1,10 @@
+### firewall (windows defender with advanced security)
+### -> New outbound rule wizard 
+### -> provide path to steam.exe 
+### -> block the connection 
+### -> all checkboxex 
+### -> Name="Steamshare"
+
 netsh advfirewall firewall set rule name="Steamshare" new enable=yes
+
 pause
