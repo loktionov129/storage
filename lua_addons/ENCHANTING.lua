72787156e86b2a97455e09d95f01c99c30089fcc
Aleksandr Loktionov <loktionov129@gmail.com>
2025-12-05 14:01:31 +0700

    Create ENCHANTING.lua

---
+++ lua_addons/ENCHANTING.lua
@@ -0,0 +1,48 @@
+MACRO 6 "_2h" INV_Misc_QuestionMark
+#showtooltip 4 1
+/click TradeSkillCreateButton
+/use 4 1
+/click StaticPopup1Button1
+END
+MACRO 3 "_boots" INV_Misc_QuestionMark
+#showtooltip 8
+/click TradeSkillCreateButton
+/use 8
+/click StaticPopup1Button1
+END
+MACRO 1 "_brace" INV_Misc_QuestionMark
+#showtooltip 9
+/click TradeSkillCreateButton
+/use 9
+/click StaticPopup1Button1
+END
+MACRO 2 "_chest" INV_Misc_QuestionMark
+#showtooltip 5
+/click TradeSkillCreateButton
+/use 5
+/click StaticPopup1Button1
+END
+MACRO 7 "_cloak" INV_Misc_QuestionMark
+#showtooltip 15
+/click TradeSkillCreateButton
+/use 15
+/click StaticPopup1Button1
+END
+MACRO 8 "_gloves" INV_Misc_QuestionMark
+#showtooltip 10
+/click TradeSkillCreateButton
+/use 10
+/click StaticPopup1Button1
+END
+MACRO 5 "_shield" INV_Misc_QuestionMark
+#showtooltip 4 2
+/click TradeSkillCreateButton
+/use 4 2
+/click StaticPopup1Button1
+END
+MACRO 4 "_weapon" INV_Misc_QuestionMark
+#showtooltip 16
+/click TradeSkillCreateButton
+/use 16
+/click StaticPopup1Button1
+END
