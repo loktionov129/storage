c83fa63104a51f251a72a351ad2efad435fca845
Aleksandr Loktionov <loktionov129@gmail.com>
2026-06-10 23:09:14 +0700

    Update ish.bash

--- os/iOS/ish.bash
+++ os/iOS/ish.bash
@@ -1,2 +1,2 @@
-echo "http://dl-cdn.alpinelinux.org/alpine/v3.19/main" >> /etc/apk/repositories
+echo "http://dl-cdn.alpinelinux.org/alpine/v3.19/main" > /etc/apk/repositories
 echo "http://dl-cdn.alpinelinux.org/alpine/v3.19/community" >> /etc/apk/repositories
